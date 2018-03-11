9800509a7bafe897270ddc3bcaf339a7cb3c7374
Markus Notti <markusnotti@gmail.com>
2018-03-10 17:01:04 -0800

    changing sha for different tar

--- dadjokes.rb
+++ dadjokes.rb
@@ -2,7 +2,7 @@ class Dadjokes < Formula
   desc "A command line tool to retrieve random dad jokes"
   homepage "https://github.com/mnotti/DadJokes"
   url "https://github.com/mnotti/DadJokes/raw/master/archive/dadjokes-v1.0.tar.gz"
-  sha256 "7fe95d174252ded293c0a35d33b861bccc86c30325c4819d44aa6f0906da3c96"
+  sha256 "1e6f14beb2299aa024fbaa88015c0c2a1a9937cdf6114b5ee966bd3d735eb502"
   version "1.0"
   
   depends_on "curl"
