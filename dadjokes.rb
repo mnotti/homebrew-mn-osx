03df2475892b526bef33309fcb8ffd18206104be
Markus Notti <markusnotti@gmail.com>
2018-03-10 17:11:23 -0800

    pls

--- dadjokes.rb
+++ dadjokes.rb
@@ -2,7 +2,7 @@ class Dadjokes < Formula
   desc "A command line tool to retrieve random dad jokes"
   homepage "https://github.com/mnotti/DadJokes"
   url "https://github.com/mnotti/DadJokes/raw/master/archive/dadjokes-v1.0.tar.gz"
-  sha256 "1e6f14beb2299aa024fbaa88015c0c2a1a9937cdf6114b5ee966bd3d735eb502"
+  sha256 "8d968292d65e745095780293290c0a019d8ef41ae611437d11c8ab4debf79f92"
   version "1.0"
   
   depends_on "curl"
